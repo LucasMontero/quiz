d9dcc0b7b3c2e4862b341434673f597aa90658a6
Lucas <lucas.montero.mauriz@gmail.com>
2015-07-11 22:50:00 +0200

    version 0.1.6 Authors

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var textTitle = "Quiz"
 
 var quizController = require('../controllers/quiz_controller.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: textTitle });
 });
 
+/* GET Authors page. */
+router.get('/Authors', function(req, res) {
+  res.render('authors', {title: textTitle});
+});
+
+/*Get quizes page . */
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
 
 module.exports = router;
